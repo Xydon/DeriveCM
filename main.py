c629743b8d50f8cfcf5ae0072100b433a0c6f0e7
Xydon <snehalkumar.singh.cse20@itbhu.ac.in>
2023-10-15 22:50:34 +0530

    added the processor

---
+++ main.py
@@ -0,0 +1,50 @@
+import spacy
+
+class ProcessorComponent:
+    def __init__(self, input_parameters):
+        self.nlp = spacy.load()
+        self.input_parameters = input_parameters
+
+    def analyze_user_story_set(self, user_stories):
+        user_story_tokens = []
+        
+        # Step 1: Use spaCy to parse each user story into tokens
+        for story in user_stories:
+            tokens = self._parse_user_story(story)
+            user_story_tokens.append(tokens)
+        
+        # Step 2: Generate Term-by-User-Story matrix
+        term_user_story_matrix = self._generate_term_user_story_matrix(user_story_tokens)
+        
+        # Step 3: Attach weights to terms
+        weighted_tokens = self._attach_weights(term_user_story_matrix)
+        
+        return weighted_tokens
+
+    def _parse_user_story(self, user_story):
+        doc = self.nlp(user_story)
+        tokens = []
+        for token in doc:
+            # Extract term, part-of-speech tag, and relationships
+            term = token.text
+            pos_tag = token.pos_
+            relationships = [child.text for child in token.children]
+            tokens.append((term, pos_tag, relationships))
+        return tokens
+
+    def _generate_term_user_story_matrix(self, user_story_tokens):
+        # Implementation to create the matrix
+        pass
+
+    def _attach_weights(self, term_user_story_matrix):
+        # Implementation to attach weights to terms
+        pass
+
+# Example Usage
+if __name__ == "__main__":
+    input_parameters = {}  # Define input parameters as needed
+    user_stories = ["User story 1 text", "User story 2 text", "User story 3 text"]
+    
+    processor = ProcessorComponent(input_parameters)
+    weighted_tokens = processor.analyze_user_story_set(user_stories)
+    print(weighted_tokens)
